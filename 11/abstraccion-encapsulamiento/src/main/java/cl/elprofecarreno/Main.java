8f420c469710947a62bd197ea138053073b4cff5
elprofecarreno@gmail.com <elprofecarreno@gmail.com>
2026-09-12 12:34:26 -0300

    add 11

---
+++ 11/abstraccion-encapsulamiento/src/main/java/cl/elprofecarreno/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cl.elprofecarreno;
+
+/**
+ *
+ * @author ccarreno
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.menu();
+
+        
+        
+    }
+}
